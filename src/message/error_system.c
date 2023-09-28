a9565edcdbde147fd9375a16e0b2c22c24ec3046
Jeremy Hurpy <jeremyhurpy@gmail.com>
2023-09-29 01:21:59 +0700

    creation message error

---
+++ src/message/error_system.c
@@ -0,0 +1,27 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   error_system.c                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jhurpy <jhurpy@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/09/29 00:57:02 by jhurpy            #+#    #+#             */
+/*   Updated: 2023/09/29 01:09:56 by jhurpy           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../../includes/execute.h"
+
+void	error_system(char *msg, int errno)
+{
+	ft_putstr_fd(msg, STDERR_FILENO);
+	strerror(errno);
+}
+
+void	error_cmd(char *cmd, char *msg)
+{
+	ft_putstr_fd("minishell: ", STDERR_FILENO);
+	ft_putstr_fd(cmd, STDERR_FILENO);
+	ft_putstr_fd(": ", STDERR_FILENO);
+	ft_putendl_fd(msg, STDERR_FILENO);
+}
