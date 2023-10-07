ec78a290a820ca28038bcc5668f7d558c3f9f18f
Jeremy Hurpy <jeremyhurpy@gmail.com>
2023-10-07 16:01:16 +0700

    modification for multiple here_doc

--- src/redirection/here_doc.c
+++ src/redirection/here_doc.c
@@ -6,13 +6,13 @@
 /*   By: jhurpy <jhurpy@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/25 21:04:25 by jhurpy            #+#    #+#             */
-/*   Updated: 2023/09/25 21:04:48 by jhurpy           ###   ########.fr       */
+/*   Updated: 2023/10/07 15:57:59 by jhurpy           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../includes/execute.h"
 
-int	creat_here_doc(char *limiter)
+static int	creat_here_doc(char *limiter)
 {
 	char	*line;
 	int		fd;
@@ -22,7 +22,7 @@ int	creat_here_doc(char *limiter)
 		return (-1);
 	while (1)
 	{
-		write(STDOUT_FILENO, "heredoc> ", 9);
+		write(STDOUT_FILENO, "> ", 9);
 		line = get_next_line(STDIN_FILENO);
 		if (ft_strncmp(line, limiter, ft_strlen(limiter)) == 0
 			&& ((ft_strlen(line) - 1) == ft_strlen(limiter)))
@@ -32,4 +32,42 @@ int	creat_here_doc(char *limiter)
 	}
 	free(line);
 	return (fd);
+}
+
+static int	check_exist_heredoc(void)
+{
+	if (access(HEREDOC_NAME, F_OK) == 0)
+	{
+		if (unlink(HEREDOC_NAME) == -1)
+			return (error_system("unlink failed\n", errno), CMD_ERROR);
+	}
+	return (CMD_OK);
+}
+
+int	execute_heredoc(t_data *data, int index)
+{
+	int		i;
+	int		fd;
+
+	if (check_exist_heredoc() != CMD_OK)
+		return (CMD_ERROR);
+	i = 0;
+	fd = -1;
+	while (data->cmd[index].limiters[i])
+	{
+		if (fd != -1)
+			close(fd);
+		fd = creat_here_doc(data->cmd[index].limiters[i]);
+		if (fd == -1)
+			return (error_system("create heredoc failed\n", errno), CMD_ERROR);
+		i++;
+	}
+	if (data->cmd[index].here_doc_in == true)
+	{
+		if (dup_files(fd, STDIN_FILENO) != CMD_OK)
+			return (CMD_ERROR);
+	}
+	close(fd);
+	unlink(HEREDOC_NAME);
+	return (CMD_OK);
 }
